07a78a76183f4493cc6fde0e23acc7269674e05c
Torres <carlost.cesarf@gmail.com>
2021-09-06 22:41:22 -0600

    Hola mundo y nuevos programas

---
+++ codigoprueba/CodigoDoble/codigodoble.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main ()
+
+{
+std::cout <<"Intrudoduzca un numero: " ;
+int  x{};
+std :: cin >> x;
+int y{2};
+std:: cout<< "ingresaste y su doble es  " << x * y << ' \ n';
+return 0;
+}
---
+++ codigoprueba/codigoprueba.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int main ()
+
+{
+std::cout <<"Intrudoduzca un numero: " ;
+int  x{};
+std :: cin >> x;
+std:: cout<< "ingresaste " << x << ' \ n';
+return 0;
+}
